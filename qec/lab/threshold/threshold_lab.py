0b113e5fabff33dcab07bd2060ece877590a74a3
AAA <adel.sohbi@gmail.com>
2025-01-01 18:55:09 +0100

    move sampling and threshold to the threshold lab

--- qec/lab/threshold/threshold_lab.py
+++ qec/lab/threshold/threshold_lab.py
@@ -12,6 +12,10 @@
 
 from __future__ import annotations
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pymatching
+
 from qec.codes.base_code import BaseCode
 
 __all__ = ["ThresholdLAB"]
@@ -22,7 +26,7 @@ class ThresholdLAB:
     A class for wrapping threshold calculation
     """
 
-    __slots__ = ("_distances", "_error_rate", "_code")
+    __slots__ = ("_distances", "_error_rates", "_code", "_collected_stats")
 
     def __init__(
         self, code: BaseCode, distances: list[int], error_rates: list[float]
@@ -38,6 +42,7 @@ class ThresholdLAB:
         self._distances = distances
         self._code = code
         self._error_rates = error_rates
+        self._collected_stats = {}
 
     @property
     def distances(self) -> list[int]:
@@ -51,7 +56,7 @@ class ThresholdLAB:
         r"""
         The error rates.
         """
-        return self._error_rate
+        return self._error_rates
 
     @property
     def code(self) -> BaseCode:
@@ -59,3 +64,83 @@ class ThresholdLAB:
         The code.
         """
         return self._code
+
+    @property
+    def collected_stats(self) -> dict:
+        r"""
+        The collected stats during sampling.
+        """
+        return self._collected_stats
+
+    def compute_logical_errors(self, code: BaseCode, num_shots: int) -> int:
+        r"""
+        Sample the memory circuit and return the number of errors.
+
+        :param num_shots: The number of samples.
+        """
+
+        # Sample the memory circuit
+        sampler = code.memory_circuit.compile_detector_sampler()
+        detection_events, observable_flips = sampler.sample(
+            num_shots, separate_observables=True
+        )
+
+        # Configure the decoder using the memory circuit then run the decoder
+        detector_error_model = code.memory_circuit.detector_error_model(
+            decompose_errors=False
+        )
+
+        matcher = pymatching.Matching.from_detector_error_model(detector_error_model)
+        predictions = matcher.decode_batch(detection_events)
+
+        # Count the number of errors
+        num_errors = 0
+        for shot in range(num_shots):
+            actual_for_shot = observable_flips[shot]
+            predicted_for_shot = predictions[shot]
+            if not np.array_equal(actual_for_shot, predicted_for_shot):
+                num_errors += 1
+        return num_errors
+
+    def collect_stats(self, num_shots: int) -> None:
+        r""" """
+
+        for distance in self.distances:
+
+            temp_logical_error_rate = []
+
+            for prob_error in self.error_rates:
+
+                code = self.code(
+                    distance=distance,
+                    depolarize1_rate=prob_error,
+                    depolarize2_rate=prob_error,
+                )
+                code.build_memory_circuit(number_of_rounds=distance * 3)
+                num_errors_sampled = self.compute_logical_errors(
+                    code=code, num_shots=num_shots
+                )
+                temp_logical_error_rate.append(num_errors_sampled / num_shots)
+
+            self._collected_stats[distance] = temp_logical_error_rate
+
+    def plot_stats(self) -> None:
+        r""" """
+
+        fig, ax = plt.subplots(1, 1)
+
+        for distance in self.collected_stats.keys():
+            ax.plot(
+                self.error_rates, self.collected_stats[distance], label=f"d={distance}"
+            )
+
+        # ax.set_ylim(1e-3, 5e-1)
+        # ax.set_xlim(2e-2, 1e-1)
+        ax.loglog()
+        ax.set_title("Repetition Code Error Rates")
+        ax.set_xlabel("Phyical Error Rate")
+        ax.set_ylabel("Logical Error Rate")
+        ax.grid(which="major")
+        ax.grid(which="minor")
+        ax.legend()
+        fig.set_dpi(120)
